0d96435156b04bc68c9186880e8e886c9b91bb73
Felipe Ribeiro <ipemateusribeiro@gmail.com>
2024-05-16 20:59:01 -0300

    arrumar codigo da classe consumiveis

--- src/ppoo/seuJogo/Consumivel.java
+++ src/ppoo/seuJogo/Consumivel.java
@@ -1,5 +1,14 @@
 package ppoo.seuJogo;
 
-public class Consumivel {
+public class Consumivel extends Item {
+    private int usos;
 
+    public Consumivel(String nome, String descricao, int usos) {
+        super(nome, descricao);
+        this.usos = usos;
+    }
+
+    public int getUsos() {
+        return usos;
+    }
 }
